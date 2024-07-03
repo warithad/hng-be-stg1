515917e780aee710ef9b875dffa96cc1c66cc9b2
Abdulwarith <warith.ad02@gmail.com>
2024-07-03 22:13:50 +0100

    Refactor code to specifications

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const app = express()
 const port = process.env.PORT || 3000
 const axios = require('axios')
 
-
 async function getWeather(ip){
     try {
-        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${ip}`)
+        const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${ip}`)
         const city = response.data.location.name;
         const temp = response.data.current.temp_c;
         return {city, temp}
@@ -20,13 +19,18 @@ async function getWeather(ip){
 app.get('/api/hello', async (req, res)=>{
     try{
         const visitor_name = req.query.visitor_name
-        const client_ip = req.ip
-        const {city, temp} = await getWeather(client_ip)
 
+        let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+        const {city, temp} = await getWeather(ip)
         const greeting = `Hello, ${visitor_name}!, the temperature is ${temp} degrees Celcius in ${city}`
-
+        
+        if(ip == '::1'){
+            ip = '127.0.0.1'
+        }
+        
         res.json({
-            client_ip,
+            client_ip: ip,
             location: city,
             greeting
         })
